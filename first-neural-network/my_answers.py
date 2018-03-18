2c2816ec5364f93b64e638d114f9bac31c54cb49
Teo <tntath@gmail.com>
2018-03-18 02:20:57 +0200

    Change number of hidden layers, output layer and learnrate

--- first-neural-network/my_answers.py
+++ first-neural-network/my_answers.py
@@ -141,6 +141,6 @@ class NeuralNetwork(object):
 # Set your hyperparameters here
 ##########################################################
 iterations = 2000
-learning_rate = 0.5
-hidden_nodes = 23
-output_nodes = 3
+learning_rate = 1
+hidden_nodes = 12
+output_nodes = 1
